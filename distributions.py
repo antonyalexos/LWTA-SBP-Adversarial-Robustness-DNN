08fe565d1b86d8dfde38bab210d321eee2b43260
Unknown <antony_dc@yahoo.gr>
2020-04-17 20:43:30 +0200

    added our layers

---
+++ distributions.py
@@ -0,0 +1,211 @@
+# -*- coding: utf-8 -*-
+"""
+Some helper functions for the distributions. 
+
+@author: Konstantinos Panagiotis Panousis
+National and Kapodistrian University of Athens
+"""
+
+import tensorflow as tf
+import numpy as np
+import tensorflow_probability as tfp
+tfd = tfp.distributions
+Normal = tfd.Normal
+Beta = tfd.Beta
+
+
+# =============================================================================
+# Some helper functions
+# =============================================================================
+    
+def bin_concrete_sample(a, temp, eps=1e-8):
+    """" 
+    Sample from the binary concrete distribution
+    """
+    U = tf.random.uniform(tf.shape(a), minval = 0., maxval=1.)
+    L = tf.math.log(U+eps) - tf.math.log(1.-U+eps) 
+    X = tf.nn.sigmoid((L + tf.math.log(a))/temp)
+    
+    return tf.clip_by_value(X, 1e-4, 1.-1e-4)
+
+def concrete_sample(a, temp, eps = 1e-8):
+    """
+    Sample from the Concrete distribution
+    """
+    U = tf.random.uniform(tf.shape(a), minval = 0., maxval=1.)
+    G = - tf.math.log(-tf.math.log(U+eps)+eps)
+    t = (tf.math.log(a) + G)/temp 
+    out = tf.nn.softmax(t,-1)
+    out += eps
+    out /= tf.reduce_sum(out, -1, keepdims=True)
+    return out*tf.stop_gradient(tf.cast(a>0., tf.float32))
+    
+def bin_concrete_kl(pr_a, post_a, post_temp, post_sample):
+    """
+    Calculate the binary concrete kl using the sample
+    """
+    p_log_prob = bin_concrete_log_mass(pr_a, post_temp, post_sample)
+    q_log_prob = bin_concrete_log_mass(post_a,post_temp, post_sample)
+   
+    return -(p_log_prob - q_log_prob)
+   
+
+def concrete_kl(pr_a, post_a, post_sample):
+    """
+    Calculate the KL between two relaxed discrete distributions, using MC samples.
+    This approach follows " The concrete distribution: A continuous relaxation of 
+    discrete random variables" [Maddison et al.] and the rationale for this approximation
+    can be found in eqs (20)-(22)
+    
+    Parameters:
+        pr: tensorflow distribution
+            The prior discrete distribution.
+        post: tensorflow distribution
+            The posterior discrete distribution
+            
+    Returns:
+        kl: float
+            The KL divergence between the prior and the posterior discrete relaxations
+    """
+
+    p_log_prob = tf.math.log(pr_a)
+    q_log_prob = tf.math.log(post_a+1e-4)
+   
+    return -(p_log_prob - q_log_prob)
+
+
+def kumaraswamy_sample(conc1, conc0, sample_shape):
+    x = tf.random.uniform(sample_shape, minval=0.01, maxval=0.99)
+        
+    q_u = (1-(1-x)**(1./conc0))**(1./conc1)
+    
+    return q_u
+
+def kumaraswamy_log_pdf(a, b, x):
+    return tf.math.log(a) +tf.math.log(b) + (a-1.)*tf.math.log(x)+ (b-1.)*tf.math.log(1.-x**a)
+
+def kumaraswamy_kl(prior_alpha, prior_beta,a,b, x):
+    """
+    Implementation of the KL distribution between a Beta and a Kumaraswamy distribution.
+    Code refactored from the paper "Stick breaking DGMs". Therein they used 10 terms to 
+    approximate the infinite taylor series.
+    
+    Parameters:
+        prior_alpha: float/1d, 2d
+            The parameter \alpha  of a prior distribution Beta(\alpha,\beta).
+        prior_beta: float/1d, 2d
+            The parameter \beta of a prior distribution Beta(\alpha, \beta).
+        a: float/1d,2d
+            The parameter a of a posterior distribution Kumaraswamy(a,b).
+        b: float/1d, 2d
+            The parameter b of a posterior distribution Kumaraswamy(a,b).
+            
+    Returns:
+        kl: float
+            The KL divergence between Beta and Kumaraswamy with given parameters.
+    
+    """
+    
+    q_log_prob = kumaraswamy_log_pdf(a, b, x)
+    p_log_prob  = Beta(prior_alpha, prior_beta).log_prob(x)
+
+    return -(p_log_prob-q_log_prob)
+
+def normal_kl(m1,s1,m2,s2, sample):
+    p_log_prob = Normal(m1, s1).log_prob(sample)
+    q_log_prob = Normal(m2, s2).log_prob(sample)
+   
+    return  -(p_log_prob - q_log_prob)
+
+#####################
+## EXTRA
+#########
+
+def sample_lognormal(mu, sigma):
+    U = tf.random_normal(tf.shape(mu))
+    normal_sample = mu + U*sigma
+    log_normal_sample = tf.exp(normal_sample)
+    
+    return tf.clip_by_value(log_normal_sample, 1e-3, log_normal_sample)
+
+def lognormal_kl(mu, sigma):
+    return 0.5*(mu**2 + sigma**2 -1. ) - 2*tf.math.log(sigma)
+
+def exponential_sample(rate, eps = 1e-8):
+    U = tf.random_uniform(tf.shape(rate), minval = np.finfo(np.float32).tiny, maxval=1.)
+    
+    return -tf.math.log(U+eps)/(rate + eps)
+
+def exponential_kl(rate0, rate):
+    return tf.math.log(rate) - tf.math.log(rate0) + rate0/rate - 1.
+
+
+def sas_kl(alpha, gamma, mu, sigma ):
+    # maybe it's not alpha and it's alpha/alpha+1 and the same for gamma
+    safe_one_minus_alpha = tf.clip_by_value(1.-alpha, 1e-3, 1.-1e-3)
+    safe_alpha = tf.clip_by_value(alpha, 1e-2, 1.-1e-3)
+    
+    return 0.5*gamma*(-1.- 2*tf.math.log(sigma) + tf.square(mu) + tf.square(sigma))\
+            + (1.-gamma)* (tf.math.log(1.-gamma) - tf.math.log(safe_one_minus_alpha))\
+              + gamma*(tf.math.log(gamma)-tf.math.log(safe_alpha))
+            
+def sas_kl_2(mu, sigma, post_sample):
+    kl_w = 0.5*post_sample *( -1.- 2*tf.math.log(sigma) + tf.square(mu) + tf.square(sigma))
+    #kl_z = bin_concrete_kl(alpha, 0.5, gamma, 0.67, post_sample )
+    
+    return kl_w 
+
+
+def concrete_mass(a, temp, x):
+    # it's the log prob of the exp relaxed, so we exp it to take the log prob
+    # of the relaxed
+    n= tf.cast(tf.shape(a)[-1], tf.float32)
+    log_norm = (tf.lgamma(n)
+                      + (n - 1.)
+                      * tf.math.log(temp))
+    
+    log_un = tf.nn.log_softmax(tf.math.log(a+1e-4) -x*temp)
+    log_un = tf.reduce_sum(log_un,-1, keep_dims=True)
+    
+    pr = tf.clip_by_value(log_norm + log_un, -10., -1e-2)
+         
+    return tf.exp(pr)
+
+
+def bin_concrete_log_mass(a, temp, x):
+    log_pr = tf.math.log(temp) + tf.math.log(a + 1e-4 ) + (-temp-1) * tf.math.log(x) + (-temp-1)*tf.math.log(1-x)
+    log_pr -= 2 * (tf.math.log(a + 1e-4) - temp* tf.math.log(x) - temp*tf.math.log(1-x))
+    
+    return log_pr
+
+def beta_function(a,b):
+    """
+    Calculation of the Beta function using the lgamma (log gamma) implementation of tf.
+    
+    Parameters:
+        a: 1d or 2d tensor
+            The first parameter of the beta function
+        b: 1d or 2d tensor
+            The second parameter of the beta function
+            
+    Returns:
+        out: same as input size
+            The calculated beta function for given a and b
+    """
+    
+    return tf.exp(tf.lgamma(a) + tf.lgamma(b) - tf.lgamma(a+b))
+
+def _log_prob(loc, scale, x):
+    return _log_unnormalized_prob(loc, scale, x) - _log_normalization(scale)
+
+def _log_unnormalized_prob(loc,scale, x):
+    return -0.5 * tf.square(_z(loc, scale, x))
+
+#missing the log(2pi) term since we dont really care
+def _log_normalization(scale):
+    return  tf.math.log(scale)
+
+def _z(loc, scale, x):
+    """Standardize input `x` to a unit normal."""
+    return (x - loc) / scale
+
---
+++ sbp_lwta_con2d_layer.py
@@ -0,0 +1,224 @@
+from .distributions import normal_kl, bin_concrete_kl, concrete_kl, kumaraswamy_kl
+from .distributions import kumaraswamy_sample, bin_concrete_sample, concrete_sample
+
+import tensorflow as tf
+from tensorflow import keras
+import tensorflow_probability as tfp
+tfd = tfp.distributions
+
+class SB_Conv2d(tf.keras.layers.Layer):
+
+  def __init__(self, ksize, padding='SAME', strides=[1,1,1,1], bias = True, sbp=False, temp_bern=0.67, temp_cat=0.5, activation='lwta'):
+    super(SB_Conv2d, self).__init__()
+    self.tau = 1e-2
+    self.ksize = ksize
+    self.padding = padding
+    self.strides = strides
+    self.bias = bias
+    self.sbp = sbp
+    self.temp_bern = temp_bern
+    self.temp_cat = temp_cat
+    self.activation = activation
+ 
+    # K  = ksize[-2]
+    # U  = ksize[-1]
+
+  def build(self, input_shape):
+      
+    self.mW = self.add_weight(shape=(self.ksize[0], self.ksize[1], input_shape[3], self.ksize[-2]*self.ksize[-1]),
+                              initializer = tf.compat.v1.keras.initializers.glorot_normal(),
+                              trainable=True,
+                              dtype= tf.float32,
+                              name='mW1')
+    
+
+    
+    self.sW = self.add_weight(shape=(self.ksize[0], self.ksize[1],  input_shape[3], self.ksize[-2]*self.ksize[-1]),
+                              trainable=True,
+                              initializer=tf.constant_initializer(-5.),
+                              constraint = lambda x: tf.clip_by_value(x, -7., x ),
+                              dtype= tf.float32,
+                              name='sW1')
+    
+    # variables and construction for the stick breaking process
+    if self.sbp:
+      
+      # posterior concentrations for the Kumaraswamy distribution
+      self.conc1 = self.add_weight(shape = ([self.ksize[-2]]),
+                                   initializer = tf.constant_initializer(3.),
+                                   constraint=lambda x: tf.clip_by_value(x, -6., x),
+                                   dtype = tf.float32,
+                                   trainable=True,
+                                   name = 'sb_t_u_1')
+            
+      self.conc0 = self.add_weight(shape = ([self.ksize[-2]]),
+                                  initializer = tf.constant_initializer(1.),
+                                  constraint=lambda x: tf.clip_by_value(x, -6., x),
+                                  dtype = tf.float32,
+                                   trainable=True,
+                                  name = 'sb_t_u_2')
+
+      # posterior bernooulli (relaxed) probabilities
+      self.t_pi = self.add_weight(shape = ([self.ksize[-2]]),
+                                  initializer =  tf.compat.v1.initializers.random_uniform(-5., 1.),
+                                  constraint = lambda x: tf.clip_by_value(x, -7., 600.),\
+                                  dtype = tf.float32,
+                                  trainable=True,
+                                  name = 'sb_t_pi')
+    
+    self.biases=0.
+    if self.bias:
+        self.biases = self.add_weight(shape=(self.ksize[-2]*self.ksize[-1],),
+                               initializer=tf.constant_initializer(0.0),
+                               trainable=True,
+                               name='bias')
+            
+    self.built = True 
+    
+  def call(self,inputs,training=None):
+      
+    sW_softplus = tf.nn.softplus(self.sW)
+    
+    if training:
+
+      layer_loss = 0.
+      z = 1.
+      # reparametrizable normal sample
+      eps = tf.stop_gradient(tf.random.normal(self.mW.get_shape()))
+      W = self.mW + eps*sW_softplus
+      
+      re = tf.ones_like(W)
+      
+      # stick breaking construction
+      if self.sbp==True:
+          
+        conc1_softplus = tf.nn.softplus(self.conc1)
+        conc0_softplus= tf.nn.softplus(self.conc0)
+        
+        
+        # stick breaking construction
+        q_u = kumaraswamy_sample(conc1_softplus, conc0_softplus, sample_shape = [inputs.get_shape()[1],self.ksize[-2]])
+        pi = tf.math.cumprod(q_u)
+
+        # posterior bernooulli (relaxed) probabilities
+        t_pi_sigmoid = tf.nn.sigmoid(self.t_pi)
+        
+        z_sample = bin_concrete_sample(t_pi_sigmoid, self.temp_bern)
+        z = tf.tile(z_sample,[self.ksize[-1]])
+        re = z*W
+                      
+        kl_sticks = tf.reduce_sum(kumaraswamy_kl(tf.ones_like(conc1_softplus), tf.ones_like(conc0_softplus),
+                                                conc1_softplus, conc0_softplus, q_u))
+        kl_z = tf.reduce_sum(bin_concrete_kl(pi, t_pi_sigmoid, self.temp_bern, z_sample))
+      
+        tf.compat.v1.add_to_collection('kl_loss', kl_sticks)
+        tf.compat.v1.add_to_collection('kl_loss', kl_z)
+          
+        layer_loss = layer_loss + tf.math.reduce_mean(kl_sticks)/60000
+        layer_loss = layer_loss + tf.math.reduce_mean(kl_z)/60000
+          
+        tf.compat.v2.summary.scalar('kl_sticks', kl_sticks)
+        tf.compat.v2.summary.scalar('kl_z', kl_z)
+          
+        # if probability of activation is smaller than tau, it's inactive
+        tf.compat.v2.summary.scalar('sparsity', tf.reduce_sum(tf.cast(tf.greater(t_pi_sigmoid/(1.+t_pi_sigmoid), self.tau), tf.float32))*self.ksize[-1])
+        # spasrity = tf.reduce_sum(tf.cast(tf.greater(t_pi_sigmoid/(1.+t_pi_sigmoid), self.tau), tf.float32))*self.ksize[-1]
+        
+          
+      # add the kl terms to the collection
+      # kl_weights = tf.reduce_sum(normal_kl(tf.zeros_like(self.mW), tf.ones_like(sW_softplus), \
+                                            # self.mW, sW_softplus, W))
+                                            
+      kl_weights = - 0.5 * tf.reduce_mean(2*sW_softplus - tf.square(self.mW) - sW_softplus**2 + 1, name = 'kl_weights')
+      
+
+      tf.compat.v1.add_to_collection('losses',  kl_weights)
+      tf.compat.v2.summary.scalar('kl_weights', kl_weights)
+                   
+      layer_loss = layer_loss + tf.math.reduce_mean(kl_weights)/60000
+           
+      # convolution operation
+      lam = tf.nn.conv2d(inputs, re, strides=self.strides, padding = self.padding) + self.biases
+
+      if self.activation=='lwta':
+        assert self.ksize[-1]>1, 'The number of competing units should be larger than 1'
+
+      # reshape weight to calculate probabilities
+        lam_re = tf.reshape(lam, [-1, lam.get_shape()[1], lam.get_shape()[2], self.ksize[-2], self.ksize[-1]])
+      
+        prbs = tf.nn.softmax(lam_re) + 1e-5
+        prbs /= tf.reduce_sum(input_tensor=prbs, axis=-1, keepdims=True)
+                  
+        # draw relaxed sample and apply activation
+        xi = concrete_sample(prbs, self.temp_cat)
+        
+        #apply activation
+        out = lam_re * xi
+        out = tf.reshape(out, tf.shape(input=lam))
+                  
+        # add the relative kl terms
+        kl_xi = tf.reduce_mean(input_tensor=tf.reduce_sum(input_tensor=concrete_kl(tf.ones_like(lam_re)/self.ksize[-1], prbs, xi), axis=[1]))
+              
+        tf.compat.v1.add_to_collection('kl_loss', kl_xi)
+        tf.compat.v2.summary.scalar('kl_xi', kl_xi)
+        
+        layer_loss = layer_loss + tf.math.reduce_mean(kl_xi)/60000
+        
+      elif self.activation == 'relu':
+        out = tf.nn.relu(lam)
+      elif self.activation=='maxout':
+        lam_re =  tf.reshape(lam, [-1,lam.get_shape()[1], lam.get_shape()[2],self.ksize[-2],self.ksize[-1]])
+        out = tf.reduce_max(lam_re, -1, keepdims=False) 
+      elif self.activation=='none':
+        out = lam
+      else:
+        print('Activation:', self.activation, 'not implemented.')
+        out = lam
+
+    else:
+        
+        re = tf.ones_like(self.mW)
+        z = 1.
+        layer_loss = 0.
+        
+        # if sbp is active calculate mask and draw samples
+        if self.sbp:
+            
+            # posterior probabilities z
+            t_pi_sigmoid = tf.nn.sigmoid(self.t_pi)
+            
+            mask = tf.cast(tf.greater(t_pi_sigmoid, self.tau), tf.float32)
+            z = tfd.Bernoulli(probs = mask*t_pi_sigmoid, name="q_z_test", dtype=tf.float32).sample()
+            z = tf.tile(z, [self.ksize[-1]])
+            re = tf.tile(mask*t_pi_sigmoid,[self.ksize[-1]])
+        
+        # convolution operation
+        lam = tf.nn.conv2d(inputs, re * self.mW,  strides=self.strides, padding = self.padding) + self.biases
+        
+        if self.activation == 'lwta':
+            # calculate probabilities of activation
+            lam_re = tf.reshape(lam, [-1, lam.get_shape()[1], lam.get_shape()[2], self.ksize[-2],self.ksize[-1]])
+            prbs = tf.nn.softmax(lam_re) + 1e-5
+            prbs /= tf.reduce_sum(input_tensor=prbs,axis=-1, keepdims=True)
+            
+            # draw sample for activated units
+            out = lam_re * concrete_sample(prbs, 0.01)
+            out = tf.reshape(out, tf.shape(input=lam))
+            
+        elif self.activation == 'relu':
+            # apply relu
+            out = tf.nn.relu(lam)
+        
+        elif self.activation=='maxout':
+            # apply maxout operation
+            lam_re = tf.reshape(lam, [-1, lam.get_shape()[1], lam.get_shape()[2], self.ksize[-2], self.ksize[-1]])
+            out = tf.reduce_max(input_tensor=lam_re, axis=-1)
+        elif self.activation=='none':
+            out = lam
+        else:
+            print('Activation:', activation,' not implemented.')
+            out = lam
+        
+    self.add_loss(layer_loss)
+      # return self.out, self.mW, self.z*self.mW, self.z*self.sW**2, self.z
+    return out
